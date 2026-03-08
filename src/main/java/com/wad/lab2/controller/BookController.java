4f59bf26edd1c419b1a2d2c9a087157005118fcd
Emilia <emiliaoana01@gmail.com>
2026-03-08 17:55:07 +0200

    removed unused imports

--- src/main/java/com/wad/lab2/controller/BookController.java
+++ src/main/java/com/wad/lab2/controller/BookController.java
@@ -1,11 +1,7 @@
 package com.wad.lab2.controller;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 //TODO: Implement BookController with appropriate endpoints for managing books
 public class BookController {
-
 
 }
